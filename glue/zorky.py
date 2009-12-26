15957d245e775121ade09ad0ca5b191ab305eecf
Eric <eric@umbra.(none)>
2009-12-25 19:14:59 -0500

    HTTP isn't working

--- glue/zorky.py
+++ glue/zorky.py
@@ -2,6 +2,8 @@ from waveapi import events
 from waveapi import model
 from waveapi import robot
 
+import urllib2
+
 from zorkyconn import *
 
 def start():
@@ -17,28 +19,25 @@ def add_blip (context, string):
 
 def self_added (properties, context):
     initial_string = start ()
-    add_blip (context, "<h1>Playing Zork</h1>\n\n%s" % initial_string)
-
-def blip_created (properties, context):
-    
+    add_blip (context, "*Playing Zork*\n\n%s" % initial_string)
 
 def blip_submitted (properties, context):
     blip = context.GetBlipById (properties["blipId"])
     text = blip.GetDocument().GetText()
     if text[0] == ">":
         command = (text.split ("\n")[0])[1:].strip()
-        add_blip (context, sendcmd (command))
+        add_blip (context, urllib2.urlopen ("http://www.google.com"))
     else:
         add_blip (context, "I did not recognize that line")
 
 if __name__ == "__main__":
+
     self_robot = robot.Robot (NAME,
                            image_url="%s/assets/icon.png" % ROOT,
                            version="1",
                            profile_url=ROOT)
 
     self_robot.RegisterHandler (events.WAVELET_SELF_ADDED, self_added)
-    self_robot.RegisterHandler (events.WAVELET_BLIP_CREATED, blip_created)
     self_robot.RegisterHandler (events.BLIP_SUBMITTED, blip_submitted)
 
     self_robot.Run ()
