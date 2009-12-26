b446c4cfa07ba5ba6a5fea37a0249538bdb5cab9
Eric <eric@umbra.(none)>
2009-12-26 14:23:07 -0500

    Wrote wrappers for zorkyconn, to turn their json data into python structures. About to fix score so that when it's an empty string (like in Wishbringer) it doesn't crash. Alex, you shall find your code edited!

--- glue/zorky.py
+++ glue/zorky.py
@@ -2,14 +2,33 @@ from waveapi import events
 from waveapi import model
 from waveapi import robot
 
-import re
-
-from zorkyconn import *
+import zorkyconn
 
 NAME = "playzorky"
 ROOT = "http://%s.appspot.com" % NAME
 
 title = ""
+
+def send_cmd (wave_id, command):
+    "Make zorkyconn.send_cmd()'s output nice"
+
+    response = zorkyconn.send_cmd (wave_id, command)
+
+    return response['content']
+
+def start (wave_id, game_name):
+    "Make zorkyconn.start()'s output nice"
+
+    response = zorkyconn.start (wave_id, game_name)
+
+    return response['content']
+
+def game_list ():
+    "Make zorkyconn.game_list()'s output nice"
+
+    return ("Possible games (choose one by starting a blip with \"/game " +
+            "<name>\"):\n" + '\n'.join (map (lambda s: s.strip(".z5"),
+                                             zorkyconn.game_list()['names'])))
 
 def struck (annos):
     struck_out = False
@@ -21,17 +40,21 @@ def struck (annos):
 
 def add_blip (context, string):
     new_blip = context.GetRootWavelet().CreateBlip()
-    new_blip.GetDocument().SetText (string)
+    new_blip.GetDocument().SetText (str(string))
 
 def self_added (properties, context):
     root_text = context.GetBlipById(context.GetRootWavelet().GetRootBlipId()).GetDocument().GetText()
     game = "list"
     for line in root_text.split("\n"):
-        if line.lower().find ("Game:") != -1:
+        if line.lower().find ("/game") != -1:
             game = line[5:].strip()
+            break
+        elif line.lower().find ("/list") != -1:
+            game = "list"
+            break
 
     if game == "list":
-        add_blip (game_list())
+        add_blip (context, game_list())
     else:
         initial_string = start (context.GetRootWavelet().GetWaveId(), game)
         add_blip (context, "Playing %s\n\n%s" % (game, initial_string))
@@ -44,11 +67,16 @@ def blip_submitted (properties, context):
     if text[0] == ">":
         if not struck (annos):
             command = (text.split ("\n")[0])[1:].strip()
-            add_blip (context, str(send_cmd (context.GetRootWavelet().GetWaveId(),
-                                         command)))
-    elif text[0] == "[":
+            response = send_cmd (context.GetRootWavelet().GetWaveId(), command)
+            add_blip (context, "> %s\n%s" % (command, response))
+    elif text[0] == "/":
         if not struck (annos):
             command = (text.split ("\n")[0])[1:].strip()
+            if command[:4] == "game":
+                game = command[5:].strip()
+                initial_string = start (context.GetRootWavelet().GetWaveId(),
+                                        game)
+                add_blip (context, "Playing %s\n\n%s" % (game, initial_string))
             
 if __name__ == "__main__":
 
